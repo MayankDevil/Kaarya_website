139a69a5793135b92ef632bf5126b8728fc9918b
मयंक <126972040+MayankDevil@users.noreply.github.com>
2024-01-06 16:37:41 +0000

    Add js/

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/*
+-	website-3 "Kaarya_website"
+-	Developed by Mayank
+-	CopyRight by https://github.com/MayankDevil/
+-	JavaScript : ./js/script.js
+*/
+try
+{
+    document.title =   'Mayank'
+}
+catch(error)
+{
+    console.log(` ERROR : ${error}`)
+}
